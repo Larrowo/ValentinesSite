5109a491feaf32d33979e053452f0684d8339fc5
Julián Astudillo <102617612+Larrowo@users.noreply.github.com>
2024-02-13 13:43:58 -0300

    Updated app
    *App was changed to be more personal
    
    *Some images were added and other deleted
    
    *Added an animation to the main container on page load

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showMessage(response) {
     noButton.style.position = "absolute";
 
     // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+    document.getElementsByClassName("image")[0].src = "images/sadcat.gif";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
@@ -20,8 +20,7 @@ function showMessage(response) {
     noButton.style.top = randomY + "px";
 
     // Update text content and hide name message
-    document.getElementById("question").textContent =
-      "The no button is just for visuals";
+    document.getElementById("question").textContent = "what do you mean no? :(";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
@@ -34,10 +33,10 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "LESTGOOO see you on the 14th babygirl 💕💕";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/gianbortion-cat.gif";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
